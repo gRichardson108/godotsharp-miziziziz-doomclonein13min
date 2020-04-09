ba9170923e39a82054392499f01333e61bbd6133
Garret Richardson <garret.richardson.481@my.csun.edu>
2020-04-08 22:26:21 -0700

    added code for movement, shooting

--- Player.cs
+++ Player.cs
@@ -28,9 +28,57 @@ public class Player : KinematicBody
 		}
 	}
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(float delta)
+	{
+		if (Input.IsActionPressed("exit"))
+		{
+			GetTree().Quit(0);
+		}
+		if (Input.IsActionPressed("restart"))
+		{
+			Kill();
+		}
+	}
+
+	public override void _PhysicsProcess(float delta)
+	{
+		var moveVec = new Vector3();
+		if (Input.IsActionPressed("move_forwards"))
+		{
+			moveVec.z -= 1;
+		}
+		if (Input.IsActionPressed("move_backwards"))
+		{
+			moveVec.z += 1;
+		}
+		if (Input.IsActionPressed("move_left"))
+		{
+			moveVec.x -= 1;
+		}
+		if (Input.IsActionPressed("move_forwards"))
+		{
+			moveVec.x += 1;
+		}
+		moveVec = moveVec.Normalized(); // prevents faster diagonal movement
+		moveVec = moveVec.Rotated(new Vector3(0.0f, 1.0f, 0.0f), Rotation.y);
+		MoveAndCollide(moveVec * MOVE_SPEED * delta);
+
+		if (Input.IsActionPressed("shoot") && !animPlayer.IsPlaying())
+		{
+			animPlayer.Play("shoot");
+			// shoot the bullet as a hitscan ray
+			var coll = rayCast.GetCollider();
+			if (rayCast.IsColliding() && coll.HasMethod("kill"))
+			{
+				coll.Call("kill");
+			}
+		}
+	}
+
+	/// Represent player being killed or some similar activity that ends the game
+	public void Kill()
+	{
+		GetTree().ReloadCurrentScene();
+	}
 }
